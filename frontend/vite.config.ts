05e832a3a0af08fb15f2583007da0ad44fda17ba
tanacchi <q111026d@mail.kyutech.jp>
2025-01-12 23:29:21 +0900

    Fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,40 +1,41 @@
-import { PluginOption, defineConfig } from 'vite';
-import { crx, defineManifest } from '@crxjs/vite-plugin';
+import { PluginOption, defineConfig } from "vite";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 // Pluginは非推奨のため、PluginOptionを使う
 const viteManifestHackIssue846: PluginOption & {
   renderCrxManifest: (manifest: any, bundle: any) => void;
 } = {
   // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
-  name: 'manifestHackIssue846',
+  name: "manifestHackIssue846",
   renderCrxManifest(_manifest, bundle) {
-    bundle['manifest.json'] = bundle['.vite/manifest.json'];
-    bundle['manifest.json'].fileName = 'manifest.json';
-    delete bundle['.vite/manifest.json'];
+    bundle["manifest.json"] = bundle[".vite/manifest.json"];
+    bundle["manifest.json"].fileName = "manifest.json";
+    delete bundle[".vite/manifest.json"];
   },
 };
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: 'Chrome拡張機能の練習',
-  version: '1.0.0',
-  description: 'Zenn投稿するChrome拡張機能のサンプルです。',
+  name: "AI家庭教師くん",
+  version: "1.0.0",
+  description: "AI家庭教師くんです。",
+  permissions: ["storage", "activeTab"],
   content_scripts: [
     {
-      matches: ['https://zenn.dev/topics/*'],
-      js: ['src/content.ts'],
+      matches: ["<all_urls>"],
+      js: ["src/content.ts"],
     },
   ],
   action: {
-    default_popup: 'index.html',
-    default_icon: "kateikyoushi_woman_boy.png"
+    default_popup: "index.html",
+    default_icon: "kateikyoushi_woman_boy.png",
   },
 });
 
 export default defineConfig({
   plugins: [viteManifestHackIssue846, crx({ manifest })],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
