44e4cd5d7c4d4d08f1cdcf365d3ac235ad9563a4
rryuto5210@gmail.com <rryuto5210@gmail.com>
2025-01-18 18:21:51 +0900

    サイドパネルに要約表示

--- frontend/src/content/index.ts
+++ frontend/src/content/index.ts
@@ -1,10 +1,1 @@
-console.log("Content script loaded");
-
-// ページの情報を取得する
-const pageInfo = {
-  title: document.title,
-  url: window.location.href,
-  content: document.body.innerText,
-};
-
-console.log("Page Info:", pageInfo);
+// 不要そうなので削除
