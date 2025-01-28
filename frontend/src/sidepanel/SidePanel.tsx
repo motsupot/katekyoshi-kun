1a274100d320363fa62913a0e9662ffe7dbfd91c
tanacchi <q111026d@mail.kyutech.jp>
2025-01-29 02:44:15 +0900

    Implement QuestionCard

--- frontend/src/sidepanel/SidePanel.tsx
+++ frontend/src/sidepanel/SidePanel.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { API_HOST } from "../constants";
 import { CardProps, renderCard } from "./components";
 import { SortableList } from "../shared/components/SortableList/SortableList";
-import { useFetch, usePageInfo } from "../shared/hooks";
+import { usePageInfo } from "../shared/hooks";
 
 const SidePanelHeader: React.FC = () => <h1>AI家庭教師くん（サイドパネル）</h1>;
 
 export const SidePanel: React.FC = () => {
-  const [question, setQuestion] = useState<string>("");
+  const [, setQuestion] = useState<string>("");
   const [, setSummary] = useState<string | null>(null);
-  const [response, setResponse] = useState<string | null>(null);
+  const [, setResponse] = useState<string | null>(null);
 
   const resetStates = () => {
     setSummary(null);
@@ -18,24 +17,6 @@ export const SidePanel: React.FC = () => {
   };
 
   const pageInfo = usePageInfo(resetStates);
-
-  const {
-    data: responseData,
-    loading: isQuestionLoading,
-    fetchData: fetchAnswer,
-  } = useFetch<typeof response>(`${API_HOST}/predict`, null);
-
-  useEffect(() => {
-    if (responseData !== null) setResponse(responseData);
-  }, [responseData]);
-
-  const handleQuestion = () => {
-    if (pageInfo && question) {
-      fetchAnswer({
-        text: `「${question}」と言う質問に対して、以下の情報を踏まえて回答せよ。: ${pageInfo.content}`,
-      });
-    }
-  };
 
   const [cards, setCards] = React.useState<CardProps[]>([
     {
@@ -54,20 +35,6 @@ export const SidePanel: React.FC = () => {
   return (
     <>
       <SidePanelHeader />
-      <div>
-        <h2>質問する</h2>
-        <input
-          type="text"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          placeholder="質問を入力してください"
-        />
-        <button onClick={handleQuestion} disabled={isQuestionLoading}>
-          質問する
-        </button>
-        {isQuestionLoading && <p>考え中...</p>}
-        {response && <p>{response}</p>}
-      </div>
       <div style={{ maxWidth: 400, margin: "30px auto" }}>
         <SortableList
           items={cards}
--- frontend/src/sidepanel/components/Question.tsx
+++ frontend/src/sidepanel/components/Question.tsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardBase } from "./base";
 import { PageInfo } from "../../types/Page";
+import { useFetch } from "../../shared/hooks";
+import { API_HOST } from "../../constants";
 
 type Props = {
   pageInfo: PageInfo | null;
   model: string;
 };
 
-export const QuestionCard: React.FC<Props> = ({}) => {
+export const QuestionCard: React.FC<Props> = ({ pageInfo }) => {
   const [question, setQuestion] = useState<string>("");
-  const [answer, setAnswer] = useState("ここに回答が入ります");
+  const [answer, setAnswer] = useState<string | null>(null);
 
-  const onClick = () => {
-    setAnswer("回答しました。");
+  const {
+    data: responseData,
+    loading: isQuestionLoading,
+    fetchData: fetchAnswer,
+  } = useFetch<typeof answer>(`${API_HOST}/predict`, null);
+
+  useEffect(() => {
+    if (responseData !== null) setAnswer(responseData);
+  }, [responseData]);
+
+  const handleQuestion = () => {
+    if (pageInfo && question) {
+      fetchAnswer({
+        text: `「${question}」と言う質問に対して、以下の情報を踏まえて回答せよ。: ${pageInfo.content}`,
+      });
+    }
   };
 
   return (
@@ -23,7 +39,7 @@ export const QuestionCard: React.FC<Props> = ({}) => {
           onChange={(e) => setQuestion(e.target.value)}
           placeholder="質問を入力してください"
         />
-      <button onClick={onClick}>質問する</button>
+      <button onClick={handleQuestion}>質問する</button>
       <div
         style={{
           height: "200px",
@@ -37,7 +53,7 @@ export const QuestionCard: React.FC<Props> = ({}) => {
           borderRadius: "4px",
         }}
       >
-        {answer}
+        {isQuestionLoading ? "考え中..." : answer ?? "ここに回答が入ります"}
       </div>
     </Card>
   );
