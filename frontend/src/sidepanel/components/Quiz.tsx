6c2fad1cc663b3acf1f830101bef7e98d8f826d5
tanacchi <q111026d@mail.kyutech.jp>
2025-02-08 14:23:54 +0900

    Fetch question & feedback

--- frontend/src/sidepanel/components/Quiz.tsx
+++ frontend/src/sidepanel/components/Quiz.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PageInfo } from "../../types/Page";
 import { Card, CardBase } from "./base";
+import { useFetch } from "../../shared/hooks";
+import { API_HOST } from "../../constants";
 
 type Props = {
   pageInfo: PageInfo | null;
@@ -9,15 +11,36 @@ type Props = {
 export const QuizCard: React.FC<Props> = ({ pageInfo }) => {
   const [questionText, setQuestionText] = useState<string | null>("ああああ");
   const [answerText, setAnswerText] = useState<string>("");
+  const [feedback, setFeedback] = useState<string>(null);
   const handleAnswer = () => {
     alert(`ans: ${answerText}`);
-  }
+  };
+
+  // クイズの問題を取得.
+  const {
+    data: resQuestion,
+    loading: isQuestionLoading,
+    fetchData: fetchQuestion,
+  } = useFetch<string | null>(`${API_HOST}/predict`, null);
+
+  useEffect(() => {
+
+  }, [resQuestion])
+
+  // 解答に対する答え合わせ（フィードバック）
+  const {
+    data: resFeedback,
+    loading: isFeekbackLoading,
+    fetchData: fetchFeedback,
+  } = useFetch<string | null>(`${API_HOST}/predict`, null);
+
+  useEffect(() => {
+
+  }, [resFeedback])
 
   return (
     <Card title="クイズで理解度チェック">
-      {questionText && <div>
-        問題：{questionText}
-      </div>}
+      {questionText && <div>問題：{questionText}</div>}
       <textarea
         value={answerText}
         disabled={questionText == null}
@@ -27,6 +50,9 @@ export const QuizCard: React.FC<Props> = ({ pageInfo }) => {
         style={{ width: "100%", marginBottom: "4px" }}
       />
       <button onClick={handleAnswer}>送信</button>
+      <div>
+        フィードバック：{feedback}
+      </div>
     </Card>
   );
 };
