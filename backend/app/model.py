22e909c0900e6cffe61953876d2a2efcef94e7dc
tanacchi <q111026d@mail.kyutech.jp>
2025-02-08 23:08:10 +0900

    Add Bookmark model

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,3 +1,5 @@
+from enum import Enum
+from typing import Optional
 from pydantic import BaseModel
 from app.db import db
 from google.cloud import firestore
@@ -41,3 +43,38 @@ class PredictSummaryRequest(BaseModel):
     user_id: str
     content: str
     url: str
+
+
+class ChatType(str, Enum):
+    SUMMARY = "summary"
+    QUESTION = "question"
+    QUIZ = "quiz"
+    SCORING = "scoring"
+
+
+class Bookmark(BaseModel):
+    user_id: str
+    type: ChatType
+
+    def save(self):
+        # コレクション「qa_sessions」に新しいドキュメントを作成
+        doc_ref = db.collection('bookmarks').document()
+        # ドキュメントにデータを保存
+        doc_ref.set({
+            'url': self.url,
+            'user_id': self.user_id,
+            'body': self.body,
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
+        return doc_ref.id
+
+
+    def find(id: str):
+        doc_ref = db.collection('summaries').document(id)
+        doc = doc_ref.get()
+        return Summary.model_validate(doc.to_dict()) if doc.exists else None
+
+
+    def find_by(user_id: str):
+        docs = db.collection('summaries').where('user_id', '==', user_id).stream()
+        return list(map(lambda doc: Summary.model_validate(doc.to_dict()), docs))
