eeb6454248151d41f24827c429eed4d89805cc90
tanacchi <q111026d@mail.kyutech.jp>
2025-02-09 15:48:48 +0900

    Implement /bookmarks/quiz

--- backend/app/bookmarks/router.py
+++ backend/app/bookmarks/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from grpc import Status
-from app.model import Bookmark, BookmarkRegisterRequest, ChatType, Summary
+from app.model import Bookmark, BookmarkRegisterRequest, ChatType, Quiz, Summary
 
 
 router = APIRouter()
@@ -22,7 +22,7 @@ async def bookmark_summary(entry: BookmarkRegisterRequest):
     if summary is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Invalid summary not found."
+            detail="Summary not found."
         )
     if summary.user_id != entry.user_id:
         raise HTTPException(
@@ -41,17 +41,17 @@ async def bookmark_summary():
 
 @router.post("/quiz")
 async def bookmark_summary(entry: BookmarkRegisterRequest):
-    summary: Summary = Quiz.find(entry.id)
-    if summary is None:
+    quiz: Quiz = Quiz.find(entry.id)
+    if quiz is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Invalid summary not found."
+            detail="Quiz not found."
         )
-    if summary.user_id != entry.user_id:
+    if quiz.user_id != entry.user_id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid user."
         )
-    bookmark = Bookmark(user_id=summary.user_id, type=ChatType.SUMMARY, item_id=entry.id)
+    bookmark = Bookmark(user_id=quiz.user_id, type=ChatType.QUIZ, item_id=entry.id)
     bookmark_id = bookmark.save()
     return dict(id=bookmark_id)
