a82b9741cb5cfb60b3275cba8b54331b8a56e799
rryuto5210@gmail.com <rryuto5210@gmail.com>
2025-02-09 11:08:19 +0900

    サイドパネル・ページへの導線改善

--- frontend/src/background/index.ts
+++ frontend/src/background/index.ts
@@ -2,25 +2,32 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("AI家庭教師くんがインストールされました！");
   chrome.contextMenus.create({
-    id: "sampleMenu",
-    title: "メニューをクリック",
+    id: "pageMenu",
+    title: "AI家庭教師くんを開く",
     contexts: ["all"], // メニューを表示するコンテキスト（例: ページ, リンクなど）
   });
 });
 
 // メニュークリック時のイベントリスナーを追加
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "sampleMenu") {
-    console.log("アイコンがクリックされました！");
-  }
-  const url = ((selectionText: string | undefined) => {
-    if (selectionText == null || selectionText === "") {
-      return `dist/page/index.html`;
-    }
-    return `dist/page/index.html?selected_text=${selectionText}`;
-  })(info.selectionText);
-  if (info.selectionText)
-    chrome.tabs.create({ url }).catch((err) => console.log(err));
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tab = tabs[0];
+    chrome.sidePanel.open({
+      windowId: tab.windowId,
+      tabId: tab.id,
+    });
+  });
+});
+
+// アクションボタンをクリックしたときのイベントリスナーを追加
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tab = tabs[0];
+    chrome.sidePanel.open({
+      windowId: tab.windowId,
+      tabId: tab.id,
+    });
+  });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
